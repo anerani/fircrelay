a778bb871f2dbf3f9c6c96b93b915a43761c0b66
Tomi Juntunen <erani@iki.fi>
2017-10-23 23:02:28 +0300

    __main__.py: Change argparser indentations.

--- __main__.py
+++ __main__.py
@@ -9,57 +9,24 @@ if __name__ == "__main__":
         description='File pipe to IRC echobot.'
     )
 
-    arg_parser.add_argument(
-        '-s',
-        '--server',
-        dest="server",
-        type=str,
-        required=True,
-        help="server address"
-    )
-    arg_parser.add_argument(
-        '-n',
-        '--nick',
-        dest="nick",
-        type=str,
-        help="nickname",
-        default="ircbot"
-    )
-    arg_parser.add_argument(
-        '-p',
-        '--port',
-        dest="port",
-        type=int,
-        help="irc server port number",
-        required=True
-    )
-    arg_parser.add_argument(
-        '-c',
-        '--channel',
-        dest="channel",
-        type=str,
-        help="channel to join",
-        required=True
-    )
-    arg_parser.add_argument(
-        '-X',
-        '--irc-ssl',
-        dest="irc_ssl",
-        type=bool,
-        choices=[True, False],
-        default=True,
-        help="use SSL (default: True)"
-    )
-    arg_parser.add_argument(
-        '-f',
-        '--pipefile',
-        dest="pipe_file",
-        type=str,
-        help="path and filename to create the pipe to read from",
-        default="{0}/{1}".format(
-            os.path.dirname(os.path.abspath(__file__)),
-            "irc.pipe"
-        )
+    arg_parser.add_argument('-s', '--server', dest="server",
+                            type=str, required=True, help="server address")
+    arg_parser.add_argument('-n', '--nick', dest="nick",
+                            type=str, help="nickname", default="ircbot")
+    arg_parser.add_argument('-p', '--port', dest="port",
+                            type=int, help="irc server port number", required=True)
+    arg_parser.add_argument('-c', '--channel', dest="channel",
+                            type=str, help="channel to join", required=True)
+    arg_parser.add_argument('-X', '--irc-ssl', dest="irc_ssl",
+                            type=bool, choices=[True, False], default=True,
+                            help="use SSL (default: True)")
+    arg_parser.add_argument('-f', '--pipefile', dest="pipe_file",
+                            type=str,
+                            help="path and filename to create the pipe to read from",
+                            default="{0}/{1}".format(
+                                os.path.dirname(os.path.abspath(__file__)),
+                                "irc.pipe"
+                            )
     )
 
     args = arg_parser.parse_args()
